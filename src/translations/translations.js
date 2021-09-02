56edf779c6aa5f2e3f3d6556c120bda28ad3d744
pawelsan <pawelsan@yahoo.com>
2021-09-02 22:17:23 +0200

    change tooltip font size and text

--- src/translations/translations.js
+++ src/translations/translations.js
@@ -3,12 +3,12 @@ export const translationsPl = {
   emptyInputError: "Proszę wpisać tytuł",
   addButton: "Dodaj",
   demoteButton: "Usuń z best of the best",
-  awardButton: "Dodaj jako best of the best",
+  awardButton: "Ustaw jako best of the best",
 };
 export const translationsEn = {
   title: "List of Favourite Music",
   emptyInputError: "Please type in a title",
   addButton: "Add",
   demoteButton: "Remove from best of the best",
-  awardButton: "Add to the best of the best",
+  awardButton: "Set as the best of the best",
 };
